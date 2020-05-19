ea64bc98716bf6f7af2cabc58ea597d7ed458ab6
lizalisa <65626017+lizalisa@users.noreply.github.com>
2020-05-20 00:02:58 +0300

    script

---
+++ laba3/suzen19.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p Documents/projects/lab19/using/simple/bash/commands
+cd Documents/projects/lab19/using/simple/bash/commands
+echo "Flag is: $(ls | grep -o '[0-9a-zA-Z]\{28\}')"
